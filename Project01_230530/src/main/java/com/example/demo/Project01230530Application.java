49a4df92079b7756c0db84cc259003e097351a28
HasithaKannangara <hasitha4kannangara@gmail.com>
2023-05-30 16:51:52 +0900

    2023-05-30 commit2 hi 1

--- Project01_230530/src/main/java/com/example/demo/Project01230530Application.java
+++ Project01_230530/src/main/java/com/example/demo/Project01230530Application.java
@@ -8,7 +8,7 @@ public class Project01230530Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project01230530Application.class, args);
-		System.out.println("Hi ");
+		System.out.println("Hi 1");
 	}
 
 }
